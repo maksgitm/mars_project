bea61f5a97d24cfa97342df9d39814c117df4299
penkovme <maksik.penkov@gmail.com>
2024-03-01 18:56:00 +0300

    comm 2

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from data import db_session
 from data.users import User
-from data.jobs import Jobs
+from flask_login import LoginManager, login_user
+from data.login import LoginForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
+
+
 def main():
     db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-    for user in db_sess.query(User):
-        print(user.__str__())
     app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
